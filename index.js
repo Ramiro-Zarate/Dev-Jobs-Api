a606a055e182d664aaa19ab7d8659d7bf233c9f6
Ramiro-Zarate <ramazara13@gmail.com>
2026-04-22 12:24:40 -0300

    feat: Add jobs.json in index.js path

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import express from 'express'
+import jobs from './jobs.json' with {type: 'json'} // with type requerido pq por default no puede importar archivos y leerlos como json por seguridad
 
 const PORT = process.env.PORT ?? 1234
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.send('Hola, primer archivo de back')
+app.get('/', (req, res) => {
+    return res.send('Hola, primer archivo de back')
 })
 
-app.get('/health', (request, respone)=>{
-    return respone.json({
+app.get('/health', (req, res)=>{
+    return res.json({
         status: 'ok',
         uptime: process.uptime()
     })
+})
+
+app.get('/get-jobs', (req, res)=>{
+    return res.json(jobs)
 })
 
 app.listen(PORT, ()=>{
